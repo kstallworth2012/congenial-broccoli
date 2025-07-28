e4f6f51deedb06e85c2e755a4fb88e0d376f35b7
Kenneth Stallworth <kennethstallworth@hotmail.com>
2025-07-27 23:29:26 -0400

    Entity updates

--- src/main/java/com/mega/parts/MegaPartsApplication/domain/entities/DepartmentsEntity.java
+++ src/main/java/com/mega/parts/MegaPartsApplication/domain/entities/DepartmentsEntity.java
@@ -4,7 +4,11 @@ package com.mega.parts.MegaPartsApplication.domain.entities;
 
 
 
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,8 +19,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity
+@Table(name="departments")
 public class DepartmentsEntity{
 
+@Id
 private Long department_id;
 private String department_name;
 private String manager_id;
--- src/main/java/com/mega/parts/MegaPartsApplication/domain/entities/EmployeesEntity.java
+++ src/main/java/com/mega/parts/MegaPartsApplication/domain/entities/EmployeesEntity.java
@@ -2,7 +2,16 @@ package com.mega.parts.MegaPartsApplication.domain.entities;
 
 import com.mega.parts.MegaPartsApplication.domain.entities.DepartmentsEntity;
 import com.mega.parts.MegaPartsApplication.domain.entities.JobsEntity;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.CascadeType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,17 +22,29 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity
+@Table(name="employees")
 public class EmployeesEntity{
-private Long employee_id;
-private String first_name;
-private String last_name;
-private String email;
-private String phone_number;
-private LocalDateTime hire_date;
-private JobsEntity job_id;
-private double salary;
-private double commission_pct;
-private String manager_id;
-private DepartmentsEntity department_id;
 
+			@Id	
+			private Long employee_id;
+			private String first_name;
+			private String last_name;
+			private String email;
+			private String phone_number;
+			private LocalDateTime hire_date;
+
+
+			@OneToOne(cascade = CascadeType.ALL)
+			@JoinColumn(name = "job_id", referencedColumnName = "job_id")
+			private JobsEntity job_id;
+			private double salary;
+			private double commission_pct;
+			private String manager_id;
+			
+			
+			@OneToOne(cascade = CascadeType.ALL)
+			@JoinColumn(name = "department_id", referencedColumnName = "department_id")
+			private DepartmentsEntity department_id;
+			
 }
