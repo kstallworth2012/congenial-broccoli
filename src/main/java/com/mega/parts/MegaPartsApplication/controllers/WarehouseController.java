8e636bd4b0d7e38ff7bc27bb04d21e9f9405f893
Kenneth Stallworth <kennethstallworth@hotmail.com>
2025-08-02 08:14:46 -0400

    warehouse controller updates

--- src/main/java/com/mega/parts/MegaPartsApplication/controllers/WarehouseController.java
+++ src/main/java/com/mega/parts/MegaPartsApplication/controllers/WarehouseController.java
@@ -2,30 +2,30 @@ package com.mega.parts.MegaPartsApplication.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.mega.parts.MegaPartsApplication.domain.entities.Entity;
-import com.mega.parts.MegaPartsApplication.repositories.Repository;
+import com.mega.parts.MegaPartsApplication.domain.entities.WarehouseEntity;
+import com.mega.parts.MegaPartsApplication.repositories.WarehouseRepository;
 
 @RestController
 @RequestMapping("/api/warehouse")
 public class WarehouseController{
 
 
-		private final InventoryInventoryRepository inventoryRepository;
+		private final WarehouseRepository warehouseRepository;
 
-    public Controller(InventoryRepository _inventoryRepository) {
-        this.inventoryRepository = _inventoryRepository;
+    public WarehouseController(WarehouseRepository _warehouseRepository) {
+        this.WarehouseRepository = _warehouseRepository;
     }
 
 
     // Read All
     @GetMapping
-    public Iterable<InventoryEntity> getAllInventory() {
-        return inventoryRepository.findAll();
+    public Iterable<WarehouseEntity> getAllWarehouses() {
+        return WarehouseRepository.findAll();
     }
 
     // Read One
     @GetMapping("/{id}")
-    public ResponseEntity<InventoryEntity> getInventoryById(@PathVariable Long id) {
+    public ResponseEntity<WarehouseEntity> getWarehouseById(@PathVariable Long id) {
         return inventoryRepository.findById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
